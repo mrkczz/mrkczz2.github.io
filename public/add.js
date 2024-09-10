969140a3d5d429b18f75abb8ee83004e860409bb
mrkczz <86231814+mrkczz@users.noreply.github.com>
2024-09-10 21:29:38 +0100

    Delete public directory

--- public/add.js
+++
@@ -1,9 +0,0 @@
-let newLi = document.createElement("li");
-
-
-newLi.style.cssText = "color: blue;";
-newLi.textContent = "Helps improve blood circulation";
-
-const placer = document.querySelector(".benefits-list");
-let lebron = placer.childNodes[7];
-lebron.parentNode.insertBefore(newLi, lebron);
